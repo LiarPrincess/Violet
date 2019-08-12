d9c407d5d00fee870c8a18c1c51414504a63ee1c
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2019-08-12 21:17:50 +0200

    NonEmptyArray now conforms to BidirectionalCollection

--- Sources/Parser/NonEmptyArray.swift
+++ Sources/Parser/NonEmptyArray.swift
@@ -1,25 +1,67 @@
-internal struct NonEmptyArray<Element> {
+// swiftlint:disable force_unwrapping
 
-  internal let first: Element
-  internal let rest: [Element]
+/// Array that has at least 1 element.
+internal struct NonEmptyArray<Element>: Sequence,
+                                        Collection,
+                                        BidirectionalCollection,
+                                        CustomStringConvertible {
 
-  internal var last: Element {
-    if let l = self.rest.last {
-      return l
-    }
-    return self.first
-  }
+  internal private(set) var elements = [Element]()
+
+  internal var first: Element { return self.elements.first! }
+  internal var last:  Element { return self.elements.last! }
 
   internal init(first: Element, rest: [Element] = []) {
-    self.first = first
-    self.rest = rest
+    self.elements.append(first)
+    self.elements.append(contentsOf: rest)
+  }
+
+  internal mutating func append(_ newElement: Element) {
+    self.elements.append(newElement)
+  }
+
+  internal mutating func append<S: Sequence>(contentsOf newElements: S)
+    where S.Element == Element {
+
+    self.elements.append(contentsOf: newElements)
+  }
+
+  // MARK: - Collection
+
+  typealias Index = Array<Element>.Index
+  typealias Element = Array<Element>.Element
+  typealias Iterator = Array<Element>.Iterator
+
+  internal var startIndex: Index { return self.elements.startIndex }
+  internal var endIndex:   Index { return self.elements.endIndex }
+
+  internal subscript(index: Index) -> Iterator.Element {
+    return self.elements[index]
+  }
+
+  internal func index(after i: Index) -> Index {
+    return self.elements.index(after: i)
+  }
+
+  internal func index(before i: Index) -> Index {
+    return self.elements.index(before: i)
+  }
+
+  internal func makeIterator() -> IndexingIterator<[Element]> {
+    return self.elements.makeIterator()
+  }
+
+  // MARK: - CustomStringConvertible
+
+  internal var description: String {
+    return String(describing: self.elements)
   }
 }
 
+// MARK: - To array
+
 extension Array {
   internal init(_ nonEmpty: NonEmptyArray<Element>) {
-    self.init()
-    self.append(nonEmpty.first)
-    self.append(contentsOf: nonEmpty.rest)
+    self = nonEmpty.elements
   }
 }
