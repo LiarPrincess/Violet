ec892a24b68370f7f947ae9211c2111f35e2ac09
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-01-06 14:31:16 +0100

    Added 'Rapunzel' module

--- Package.swift
+++ Package.swift
@@ -11,39 +11,52 @@ let package = Package(
     .executable(name: "Violet", targets: ["Violet"]),
     .library(name: "VioletFramework", targets: ["VM"]),
 
-    .executable(name: "Elsa", targets: ["Elsa"])
+    .executable(name: "Elsa", targets: ["Elsa"]),
+    .library(name: "Rapunzel", targets: ["Rapunzel"])
   ],
   dependencies: [
     // We will use 'TSCUtility.ArgumentParser' from this package.
-    // Also, this is VERY old version of SPM, but it works, so whatever.
+    // Also: this is VERY old version of SPM, but it works, so whatever.
     .package(url: "https://github.com/apple/swift-package-manager", from: "0.4.0")
   ],
   targets: [
+    // Shared module that all of the other modules depend on.
     .target(name: "Core", dependencies: []),
     .testTarget(name: "CoreTests", dependencies: ["Core"]),
 
+    // String -> Tokens
     .target(name: "Lexer", dependencies: ["Core"]),
     .testTarget(name: "LexerTests", dependencies: ["Core", "Lexer"]),
 
+    // Tokens -> AST
     .target(name: "Parser", dependencies: ["Lexer"]),
-    .testTarget(name: "ParserTests", dependencies: ["Parser"]),
+    .testTarget(name: "ParserTests", dependencies: ["Parser", "Rapunzel"]),
 
+    // AST -> Bytecode
     .target(name: "Compiler", dependencies: ["Parser", "Bytecode"]),
     .testTarget(name: "CompilerTests", dependencies: ["Compiler"]),
 
+    // VM instructions
     .target(name: "Bytecode", dependencies: ["Core"]),
     .testTarget(name: "BytecodeTests", dependencies: ["Bytecode"]),
 
-    .target(name: "Objects", dependencies: ["Core", "Bytecode"]),
+    // Python objects
+    .target(name: "Objects", dependencies: ["Bytecode"]),
     .testTarget(name: "ObjectsTests", dependencies: ["Objects"]),
 
+    // Python runtime + bytecode interpretation
     .target(name: "VM", dependencies: ["Compiler", "Objects", "SPMUtility"]),
     .testTarget(name: "VMTests", dependencies: ["VM"]),
 
     // Main executable
     .target(name: "Violet", dependencies: ["VM"]),
 
-    // Elsa is our code generation tool.
-    .target(name: "Elsa")
+    // Code generation tool used for AST and bytecode
+    .target(name: "Elsa"),
+
+    // Pretty printer based on Philip Wadler idea:
+    // https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf
+    .target(name: "Rapunzel", dependencies: []),
+    .testTarget(name: "RapunzelTests", dependencies: ["Rapunzel"])
   ]
 )
