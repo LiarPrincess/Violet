2c1ba9b608ad103774524cae3ce3acfb2a1e79c3
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-02-11 21:21:43 +0100

    Added 'BigInt' tests

--- Sources/Core/BigInt.swift
+++ Sources/Core/BigInt.swift
@@ -4,45 +4,27 @@
 //   from what can be stored in available memory.
 // In Swift we dont't have such type, so for now we will just use 'Int64'.
 
+// swiftlint:disable file_length
+
 // TODO: Maybe use: https://github.com/apple/swift/blob/master/test/Prototypes/BigInt.swift
 
-extension Double {
-  public init(_ value: BigInt) {
-    self = Double(value.value)
-  }
-
-  public init?(exactly: BigInt) {
-    guard let d = Double(exactly: exactly.value) else {
-      return nil
-    }
-    self = d
-  }
-}
-
-extension BinaryInteger {
-  public init?(exactly value: BigInt) {
-    guard let v = Self(exactly: value.value) else {
-      return nil
-    }
-    self = v
-  }
-}
-
-extension String {
-  public init(_ value: BigInt, radix: Int = 10, uppercase: Bool = false) {
-    self.init(value.value, radix: radix, uppercase: uppercase)
-  }
-}
-
 public struct BigInt:
+  SignedInteger, BinaryInteger,
   Equatable, Hashable, Comparable, Strideable,
-  SignedNumeric,
   CustomStringConvertible {
 
   /// Type that will be used as a storage.
-  fileprivate typealias Storage = Int64
+  ///
+  /// IMPLEMENTATION DETAIL! DO NOT USE!
+  public typealias Storage = Int64
 
   // MARK: - Static properties
+
+  /// A type that represents the words of a binary integer.
+  public typealias Words = Storage.Words
+
+  /// A type that can represent the absolute value of any possible value of this type.
+  public typealias Magnitude = Storage.Magnitude
 
   /// The maximum representable integer in this type.
   public static let min = BigInt(Storage.min)
@@ -50,39 +32,63 @@ public struct BigInt:
   /// The minimum representable integer in this type.
   public static let max = BigInt(Storage.max)
 
-  /// The number of bits used for the underlying binary representation of
-  /// values of this type.
-  ///
-  /// For example: bit width of a `Int64` instance is 64.
-  public static let bitWidth = Storage.bitWidth
-
   // MARK: - Properties
 
-  fileprivate var value: Storage
+  /// Holds our `BigInt` value.
+  private var value: Storage
+
+  /// A collection containing the words of this value’s binary representation,
+  /// in order from the least significant to most significant.
+  public var words: Words {
+    return self.value.words
+  }
 
   /// The magnitude of this value.
   ///
   /// For any numeric value `x`, `x.magnitude` is the absolute value of `x`.
-  public var magnitude: UInt64 {
+  public var magnitude: Magnitude {
     return self.value.magnitude
   }
 
-  /// The number of leading zeros in this value's binary representation.
+  /// The number of bits used for the underlying binary representation of
+  /// values of this type.
   ///
-  /// For example, in an integer type with a `bitWidth` value of 8,
-  /// the number *31* has three leading zeros.
-  ///
-  ///     let x: Int8 = 0b0001_1111
-  ///     // x == 31
-  ///     // x.leadingZeroBitCount == 3
-  public var leadingZeroBitCount: Int {
-    self.value.leadingZeroBitCount
+  /// For example: bit width of a `Int64` instance is 64.
+  public var bitWidth: Int {
+    return self.value.bitWidth
   }
 
-  /// Minimum number of bits needed to represent this value.
+  /// The number of trailing zeros in this value’s binary representation.
+  public var trailingZeroBitCount: Int {
+    return self.value.trailingZeroBitCount
+  }
+
+  /// The number of leading zeros in this value’s binary representation.
+  public var leadingZeroBitCount: Int {
+    return self.value.leadingZeroBitCount
+  }
+
+  /// Number of bits necessary to represent self in binary.
   /// `bitLength` in Python.
+  ///
+  ///```py
+  /// >>> bin(37)
+  /// '0b100101'
+  /// >>> (37).bit_length()
+  /// 6
+  ///```
   public var minRequiredWidth: Int {
-    return BigInt.bitWidth - self.value.leadingZeroBitCount
+    if self == 0 {
+      return 0
+    }
+
+    if self > 0 {
+      return self.bitWidth - self.leadingZeroBitCount
+    }
+
+    // Reverse two complement -> the same '> 0'
+    let reverse = (~self) + 1
+    return reverse.bitWidth - reverse.leadingZeroBitCount
   }
 
   public var description: String {
@@ -91,30 +97,42 @@ public struct BigInt:
 
   // MARK: - Init
 
-  public init<T>(_ source: T) where T : BinaryInteger {
-    self.value = Int64(source)
+  public init() {
+    self.value = Storage()
   }
 
-  public init<T>(_ source: T) where T : BinaryFloatingPoint {
-    self.value = Int64(source)
+  public init(integerLiteral value: Int) {
+    self.value = Int64(value)
   }
 
-  public init(integerLiteral value: Int64) {
-    self.value = Int64(integerLiteral: value)
+  public init<T: BinaryInteger>(_ source: T) {
+    self.value = Storage(source)
   }
 
-  public init?<T>(exactly source: T) where T : BinaryInteger {
-    guard let v = Int64(exactly: source) else { return nil }
+  public init?<T: BinaryInteger>(exactly source: T) {
+    guard let v = Storage(exactly: source) else { return nil }
     self.value = v
   }
 
-  public init?<T>(exactly source: T) where T : BinaryFloatingPoint {
-    guard let v = Int64(exactly: source) else { return nil }
+  public init<T : BinaryInteger>(truncatingIfNeeded source: T) {
+    self.value = Storage(truncatingIfNeeded: source)
+  }
+
+  public init<T : BinaryInteger>(clamping source: T) {
+    self.value = Storage(clamping: source)
+  }
+
+  public init<T: BinaryFloatingPoint>(_ source: T) {
+    self.value = Storage(source)
+  }
+
+  public init?<T: BinaryFloatingPoint>(exactly source: T) {
+    guard let v = Storage(exactly: source) else { return nil }
     self.value = v
   }
 
-  public init?<S>(_ text: S, radix: Int = 10) where S : StringProtocol {
-    guard let value = Int64(text, radix: radix) else {
+  public init?<S: StringProtocol>(_ text: S, radix: Int = 10) {
+    guard let value = Storage(text, radix: radix) else {
       return nil
     }
     self.value = value
@@ -164,8 +182,16 @@ public struct BigInt:
     return BigInt(lhs.value / rhs.value)
   }
 
+  public static func /= (lhs: inout BigInt, rhs: BigInt) {
+    lhs.value /= rhs.value
+  }
+
   public static func % (lhs: BigInt, rhs: BigInt) -> BigInt {
     return BigInt(lhs.value % rhs.value)
+  }
+
+  public static func %= (lhs: inout BigInt, rhs: BigInt) {
+    lhs.value %= rhs.value
   }
 
   // MARK: - Equatable
@@ -245,18 +271,30 @@ public struct BigInt:
     lhs.value >>= rhs
   }
 
-  // MARK: - Binary operators
+  // MARK: - Bit operators
 
   public static func & (lhs: BigInt, rhs: BigInt) -> BigInt {
     return BigInt(lhs.value & rhs.value)
+  }
+
+  public static func &= (lhs: inout BigInt, rhs: BigInt) {
+    lhs.value &= rhs.value
   }
 
   public static func | (lhs: BigInt, rhs: BigInt) -> BigInt {
     return BigInt(lhs.value | rhs.value)
   }
 
+  public static func |= (lhs: inout BigInt, rhs: BigInt) {
+    lhs.value |= rhs.value
+  }
+
   public static func ^ (lhs: BigInt, rhs: BigInt) -> BigInt {
     return BigInt(lhs.value ^ rhs.value)
+  }
+
+  public static func ^= (lhs: inout BigInt, rhs: BigInt) {
+    lhs.value ^= rhs.value
   }
 
   // MARK: - Strideable
