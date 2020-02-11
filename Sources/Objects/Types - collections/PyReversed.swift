99eb0f95c250407863a1055dd25a930d2b413298
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-02-11 14:06:37 +0100

    PyReverse no longer requires '__reversed__'

--- Sources/Objects/Types - collections/PyReversed.swift
+++ Sources/Objects/Types - collections/PyReversed.swift
@@ -25,16 +25,14 @@ public class PyReversed: PyObject {
 
   // MARK: - Init
 
-  internal convenience init(sequence: PyObject, sequenceCount: Int) {
-    self.init(type: Py.types.reversed,
-              sequence: sequence,
-              sequenceCount: sequenceCount)
+  internal convenience init(sequence: PyObject, count: Int) {
+    self.init(type: Py.types.reversed, sequence: sequence, count: count)
   }
 
   /// Use only in `__new__`!
-  internal init(type: PyType, sequence: PyObject, sequenceCount: Int) {
+  internal init(type: PyType, sequence: PyObject, count: Int) {
     self.sequence = sequence
-    self.index = sequenceCount - 1
+    self.index = count - 1
     super.init(type: type)
   }
 
@@ -67,7 +65,7 @@ public class PyReversed: PyObject {
 
   // sourcery: pymethod = __next__
   internal func next() -> PyResult<PyObject> {
-    if self.index >= 0 {
+    if self.index != PyReversed.endIndex {
       switch Py.getItem(self.sequence, at: self.index) {
       case .value(let o):
         self.index -= 1
@@ -109,48 +107,62 @@ public class PyReversed: PyObject {
 
   private static func pyNew(type: PyType,
                             object: PyObject) -> PyResult<PyObject> {
-    let reverse: PyObject
+    // If we have dedicated '__reversed__' then we will use it
     switch PyReversed.call__reversed__(on: object) {
     case .value(let r):
-      reverse = r
+      return .value(r)
     case .missingMethod:
+      break // try other options
+    case .error(let e), .notCallable(let e):
+      return .error(e)
+    }
+
+    // If we have '__getitem__' and '__len__' the we can manually create reverse.
+    switch PyReversed.has__getitem__(object: object) {
+    case .value(true):
+      break // yay!
+    case .value(false):
       return .typeError("'\(object.typeName)' object is not reversible")
     case .error(let e):
       return .error(e)
     }
 
     let count: Int
-    switch Py.lengthInt(iterable: reverse) {
+    switch Py.lengthInt(iterable: object) {
     case let .value(l): count = l
     case let .error(e): return .error(e)
     }
 
     let isBuiltin = type === Py.types.reversed
     let alloca = isBuiltin ?
-      PyReversed.init(type:sequence:sequenceCount:) :
-      PyReversedHeap.init(type:sequence:sequenceCount:)
+      PyReversed.init(type:sequence:count:) :
+      PyReversedHeap.init(type:sequence:count:)
 
-    let result = alloca(type, reverse, count)
+    let result = alloca(type, object, count)
     return .value(result)
   }
 
-  private enum CallReversedResult {
-    case value(PyObject)
-    case error(PyBaseException)
-    case missingMethod
-  }
-
-  private static func call__reversed__(on object: PyObject) -> CallReversedResult {
+  private static func call__reversed__(on object: PyObject) -> CallMethodResult {
     if let owner = object as? __reversed__Owner {
       return .value(owner.reversed())
     }
 
-    switch Py.callMethod(on: object, selector: "__reversed__") {
-    case .value(let o):
-      return .value(o)
-    case .missingMethod:
-      return .missingMethod
-    case .error(let e), .notCallable(let e):
+    return Py.callMethod(on: object, selector: "__reversed__")
+  }
+
+  private static func has__getitem__(object: PyObject) -> PyResult<Bool> {
+    if object is __getitem__Owner {
+      return .value(true)
+    }
+
+    switch Py.getMethod(object: object, selector: "__getitem__") {
+    case .value:
+      return .value(true)
+    case .error(let e):
+      if e.isAttributeError {
+        return .value(false)
+      }
+
       return .error(e)
     }
   }
