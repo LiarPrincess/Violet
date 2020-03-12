4fec92d6090dba1f47ca9e9ddd09487d7c0f44d1
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-03-12 21:02:41 +0100

    Moved predefined names to 'SymbolScope'

--- Sources/Compiler/Symbol table/SymbolScope.swift
+++ Sources/Compiler/Symbol table/SymbolScope.swift
@@ -5,6 +5,24 @@ import Parser
 // In CPython:
 // Include -> symtable.h
 
+/// Predefined scope names
+internal enum SymbolScopeNames {
+
+  /// Name of the AST root scope
+  internal static let top = "top"
+  /// Name of the lambda scope
+  internal static let lambda = "lambda"
+
+  /// Name of the list comprehension scope
+  internal static let listcomp = "listcomp"
+  /// Name of the set comprehension scope
+  internal static let setcomp  = "setcomp"
+  /// Name of the dict comprehension scope
+  internal static let dictcomp = "dictcomp"
+  /// Name of the generator expression scope
+  internal static let genexpr  = "genexpr"
+}
+
 public enum ScopeType: Equatable {
   case module
   case `class`
