43af5949f8deed4d5a40a0acefb1bbb9d82e7c56
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-04-07 16:54:23 +0200

    Forgot to commit VM...

--- Sources/VM/VM.swift
+++ Sources/VM/VM.swift
@@ -1,5 +1,5 @@
-import Foundation
 import Objects
+import Foundation
 
 // swiftlint:disable:next type_name
 public final class VM: PyDelegate {
@@ -15,6 +15,26 @@ public final class VM: PyDelegate {
   public var frame: PyFrame? {
     return self.frames.last
   }
+
+  /// Currently handled exception.
+  ///
+  /// If exception was raised when another exception was beeing handled then
+  /// the new one has `__context__` set to previous exception.
+  /// (In terms of API: use `getContext()` to get to the first raised exception)
+  ///
+  /// For example:
+  /// 1. We were handling `e1`
+  /// 2. During that `e2` was raised
+  /// In such case `currentlyHandledException` is set to `e2`, but you can
+  /// get to `e1` by using `e2.__context__` (`e2.getContext()` in Swift).
+  ///
+  /// Tip 1. If `currentlyHandledException` is `nil` then we are currently
+  /// not handling any exceptions
+  /// Tip 2. To get to the first raised exception follow `getContext()` path
+  /// until you get to the exception which has `getContext()` set to `nil`.
+  ///
+  /// Required by `PyDelegate`.
+  public var currentlyHandledException: PyBaseException?
 
   public init(arguments: Arguments, environment: Environment) {
     self.arguments = arguments
