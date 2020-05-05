3b373661b37f8598a7a4cf49a64fdc487ae3ee18
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-05-05 09:54:54 +0200

    Fixed 'builtin_file' test

--- PyTests/RustPython/builtin_file.py
+++ PyTests/RustPython/builtin_file.py
@@ -1,7 +1,9 @@
-import os
+# import os
 
-from import_file import import_file
+# VIOLET: We do not have 'os' module, but we implemented our own 'basename' in 'import_file'
+
+from import_file import import_file, basename
 
 import_file()
 
-assert os.path.basename(__file__) == "builtin_file.py"
+assert basename(__file__) == "builtin_file.py"
--- PyTests/RustPython/import_file.py
+++ PyTests/RustPython/import_file.py
@@ -1,4 +1,13 @@
-import os
+# import os
+
+# VIOLET: We do not have 'os' module, so we will implement our own solution.
+def basename(path: str):
+  try:
+    slash_index = path.rindex('/')
+    name_start_index = slash_index + 1
+    return path[name_start_index:]
+  except ValueError:
+    return path
 
 def import_file():
-	assert os.path.basename(__file__) == "import_file.py"
+	assert basename(__file__) == "import_file.py"
