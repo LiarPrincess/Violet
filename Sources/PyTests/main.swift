454bb1b118f4228b2e006874973fc7ae4ce34a8b
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2020-07-15 17:49:08 +0200

    'PyTests' will run all of the '.py' files from given directory

--- Sources/PyTests/main.swift
+++ Sources/PyTests/main.swift
@@ -3,22 +3,38 @@ import VioletCore
 import VioletObjects
 import VioletVM
 
-internal var rootDir: URL = {
-  let currentFile = URL(fileURLWithPath: #file)
-  return currentFile
-    .deletingLastPathComponent()
-    .deletingLastPathComponent()
-    .deletingLastPathComponent()
-}()
+private let currentFile = URL(fileURLWithPath: #file)
 
-internal let testDir = rootDir.appendingPathComponent("PyTests")
+let rootDir = currentFile
+  .deletingLastPathComponent()
+  .deletingLastPathComponent()
+  .deletingLastPathComponent()
+
+let testDir = rootDir.appendingPathComponent("PyTests")
 
 // If we call 'open' we want to start at repository root.
 guard FileManager.default.changeCurrentDirectoryPath(rootDir.path) else {
   trap("Failed to set cwd to: '\(rootDir.path)'")
 }
 
+// MARK: - Config
+
 stopAtFirstFailedTest = true
-runRustPythonTests()
-runVioletTests()
+
+// MARK: - Rust tests
+
+try runAllTests(
+  from: testDir.appendingPathComponent("RustPython"),
+  skip: [
+    "code.py", // We do not support all properties
+    "extra_bool_eval.py" // Needs peephole optimizer
+  ]
+)
+
+// MARK: - Violet tests
+
+try runAllTests(from: testDir.appendingPathComponent("Violet"))
+
+// MARK: - Summary
+
 printSummary()
--- Sources/PyTests/runTest.swift
+++ Sources/PyTests/runTest.swift
@@ -10,6 +10,78 @@ internal var stopAtFirstFailedTest = false
 private var passedTests = [String]()
 private var failedTests = [String]()
 
+internal func runAllTests(from dir: URL, skip: [String] = []) throws {
+  var filenames = try FileManager.default.contentsOfDirectory(atPath: dir.path)
+  filenames.sort()
+
+  for filename in filenames {
+    guard filename.hasSuffix(".py") else {
+      continue
+    }
+
+    if skip.contains(filename) {
+      continue
+    }
+
+    let dirName = dir.lastPathComponent
+    let testName = "\(dirName) - \(filename)"
+
+    let file = dir.appendingPathComponent(filename)
+    runTest(testName: testName, path: file)
+  }
+}
+
+/// This will leak memory on every call!
+/// (because we do not have GC to break circular references)
+private func runTest(testName: String, path: URL) {
+  print(testName)
+
+  var arguments = Arguments()
+  let environment = Environment()
+  arguments.script = path.path
+
+  let vm = VM(arguments: arguments, environment: environment)
+  switch vm.run() {
+  case .done:
+    print("  ✔ Success")
+    passedTests.append(testName)
+    return
+
+  case .systemExit(let object):
+    let status: String = {
+      switch object {
+      case let o where o.isNone:
+        return "None"
+      case let int as PyInt:
+        return String(describing: int.value)
+      default:
+        return Py.reprOrGeneric(object: object)
+      }
+    }()
+
+    print("  ✔ Success (SystemExit: \(status))")
+    passedTests.append(testName)
+    return
+
+  case .error(let error):
+    // Try to print error to orginal 'stdout'
+    let stdout: PyTextFile
+    switch Py.sys.get__stdout__() {
+    case let .value(f): stdout = f
+    case let .error(e): trap("'__stdout__' is missing: \(e)")
+    }
+
+    // 'printRecursive' ignores any new errors
+    print("  ✖ Error:")
+    Py.printRecursive(error: error, file: stdout)
+    failedTests.append(testName)
+
+    if stopAtFirstFailedTest {
+      exit(1)
+    }
+  }
+}
+
 /// In case we need to run all of the tests, so see 'what sticks'.
 ///
 /// Just remember to comment the 'exit' on 'error' case in 'runTest' switch.
@@ -32,55 +104,4 @@ internal func printSummary() {
 
   printSummary(title: "Passed tests:", tests: passedTests)
   printSummary(title: "Failed tests:", tests: failedTests)
-}
-
-// This will leak memory on every call.
-// (because we do not have GC to break circular references)
-internal func runTest(file: URL) {
-  print(file.lastPathComponent)
-
-  var arguments = Arguments()
-  let environment = Environment()
-  arguments.script = file.path
-
-  let vm = VM(arguments: arguments, environment: environment)
-  switch vm.run() {
-  case .done:
-    print("  ✔ Success")
-    passedTests.append(file.lastPathComponent)
-    return
-
-  case .systemExit(let object):
-    let status: String = {
-      switch object {
-      case let o where o.isNone:
-        return "None"
-      case let int as PyInt:
-        return String(describing: int.value)
-      default:
-        return Py.reprOrGeneric(object: object)
-      }
-    }()
-
-    print("  ✔ Success (SystemExit: \(status))")
-    passedTests.append(file.lastPathComponent)
-    return
-
-  case .error(let error):
-    // Try to print error to orginal 'stdout'
-    let stdout: PyTextFile
-    switch Py.sys.get__stdout__() {
-    case let .value(f): stdout = f
-    case let .error(e): trap("'__stdout__' is missing: \(e)")
-    }
-
-    // 'printRecursive' ignores any new errors
-    print("  ✖ Error:")
-    Py.printRecursive(error: error, file: stdout)
-    failedTests.append(file.lastPathComponent)
-
-    if stopAtFirstFailedTest {
-      exit(1)
-    }
-  }
 }
