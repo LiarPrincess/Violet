a3c0bc142cfc99c4eb368360e51c5e03f2a59e81
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2021-06-11 14:27:50 +0200

    Updated 'Scripts/compiler_dump_test'

--- Scripts/compiler_dump_test/__main__.py
+++ Scripts/compiler_dump_test/__main__.py
@@ -2,6 +2,7 @@ import io
 import os
 import re
 import dis
+from typing import List
 
 
 def in_current_directory(file):
@@ -27,54 +28,171 @@ def print_docs(lines):
         print('///', lines[index])
         index += 1
 
-    # code object
+    # bytecode instructions
+    without_source_code_line = 13
     while index < len(lines):
         line = lines[index]
         if len(line) > 0:
-            print('///', line[13:])
+            print('///', line[without_source_code_line:])
+
         index += 1
 
 
-def print_expected(lines):
+def print_expected(lines: List[str]):
     index = 0
 
     # skip python code
     while not lines[index].startswith(' '):
         index += 1
 
-    argRegex = re.compile('.*\((.*)\)')
+    arg_value_from_parens_regex = re.compile('.*\((.*)\)')
 
     # expected
+    without_source_code_line_and_bytecode_index = 16
     while index < len(lines):
         line = lines[index]
-        if len(line) > 0:
-            instruction = line[16:]
-            argStart = instruction.find(' ')
-
-            if argStart == -1:
-                # .init(.return)
-                name = to_camel_case(instruction)
-                name = 'return' if name == 'returnValue' else name
-                name = name.replace('Subscr', 'Subscript')
-                print(f'.init(.{name}),')
-
-            else:
-                # .init(.loadConst, "none"),
-                name = to_camel_case(instruction[:argStart])
-                name = 'jumpAbsolute' if name == 'jumpForward' else name
-                name = name.replace('Attr', 'Attribute')
-
-                args = instruction[argStart:].strip()
-                f = argRegex.findall(args)
-                if f:
-                    args = f[0]
-
-                args = args[3:] if args.startswith('to ') else args
-                args = 'none' if args == 'None' else args
-
-                print(f'.init(.{name}, "{args}"),')
-
         index += 1
+
+        if not line:
+            continue
+
+        # Are we now code object?
+        if line.startswith('Disassembly of '):
+            print()
+            print(line)
+            print()
+            continue
+
+        instruction = line[without_source_code_line_and_bytecode_index:]
+
+        arg_start_index = instruction.find(' ')
+        name_end = len(instruction) if arg_start_index == -1 else arg_start_index
+        name = instruction[:name_end]
+        name = to_camel_case(name)
+        name = name.replace('Subscr', 'Subscript')
+        name = name.replace('Attr', 'Attribute')
+
+        if name == 'returnValue':
+            name = 'return'
+        if name == 'jumpForward':
+            name = 'jumpAbsolute'
+
+        args = '' if arg_start_index == -1 else instruction[arg_start_index:]
+        args = args.strip()
+
+        # Extract arg values from parens: '(0 ('Aurora')),' -> 'Aurora'
+        value_in_paren = arg_value_from_parens_regex.findall(args)
+        if value_in_paren:
+            args = value_in_paren[0]
+
+        # Jump addres
+        if args.startswith('to '):
+            args = args[3:]
+
+        # Instruction specifics
+        # (this is in the same order as cases in 'Instruction' enum)
+        if name == 'compareOp':
+            compare_type = \
+                '.equal' if args == '==' else \
+                '.notEqual' if args == '!=' else \
+                '.less' if args == '<' else \
+                '.lessEqual' if args == '<=' else \
+                '.greater' if args == '>' else \
+                '.greaterEqual' if args == '>=' else \
+                '.is' if args == 'is' else \
+                '.isNot' if args == 'is not' else \
+                '.in' if args == 'in' else \
+                '.notIn' if args == 'not in' else \
+                args
+
+            args = 'type: ' + compare_type
+
+        elif name == 'setupLoop':
+            value = int(args).__floordiv__(2)
+            args = f'loopEndLabel: CodeObject.Label(jumpAddress: {value})'
+        elif name == 'forIter':
+            value = int(args).__floordiv__(2)
+            args = f'ifEmptyLabel: CodeObject.Label(jumpAddress: {value})'
+        elif name == '':
+            value = int(args).__floordiv__(2)
+            args = f'loopStartLabel: CodeObject.Label(jumpAddress: {value})'
+
+        elif name in ('buildTuple', 'buildList', 'buildSet', 'buildMap', 'buildConstKeyMap'):
+            args = 'elementCount: ' + args
+        elif name in ('setAdd', 'listAppend', 'mapAdd'):
+            args = 'relativeStackIndex: ' + args
+        elif name in ('buildTupleUnpack', 'buildTupleUnpackWithCall', 'buildListUnpack', 'buildSetUnpack', 'buildMapUnpack', 'buildMapUnpackWithCall', 'unpackSequence'):
+            args = 'elementCount: ' + args
+
+        elif name == 'unpackEx':
+            pass  # TODO: unpackEx
+
+        elif name == 'loadConst':
+            if args == 'None':
+                args = '.none'
+            elif args.startswith('<code object'):
+                args = 'codeObject: .any'
+            args = args.replace("'", '"')  # ' -> "
+
+        elif name in ('loadName', 'storeName', 'deleteName'):
+            args = f'name: "{args}"'
+        elif name in ('loadAttribute', 'storeAttribute', 'deleteAttribute'):
+            args = f'name: "{args}"'
+        elif name in ('loadGlobal', 'storeGlobal', 'deleteGlobal'):
+            args = f'name: "{args}"'
+        elif name in ('loadFast', 'storeFast', 'deleteFast'):
+            args = f'variable: "{args}"'
+        # TODO: cell and free
+        # case loadCellOrFree(cellOrFree: MangledName)
+        # case storeCellOrFree(cellOrFree: MangledName)
+        # case deleteCellOrFree(cellOrFree: MangledName)
+        # case loadClassCell(cellOrFree: MangledName)
+
+        elif name == 'makeFunction':
+            if args == '0':
+                args = 'flags: []'
+            else:
+                args = 'flags: ' + args
+        elif name in ('callFunction', 'callFunctionKw'):
+            args = 'argumentCount: ' + args
+        elif name == 'callFunctionEx':
+            value = 'true' if args == '1' else 'false'
+            args = 'hasKeywordArguments: ' + value
+
+        elif name == 'loadMethod':
+            args = 'name: ' + args
+        elif name == 'callMethod':
+            args = 'argumentCount: ' + args
+
+        elif name in ('importName', 'importFrom'):
+            args = 'name: ' + args
+
+        elif name == 'setupExcept':
+            args = 'firstExceptLabel:' + args
+        elif name == 'setupFinally':
+            args = 'finallyStartLabel:' + args
+        elif name == 'raiseVarargs':
+            pass  # TODO: raiseVarargs
+
+        elif name == 'setupWith':
+            args = 'afterBodyLabel:' + args
+
+        elif name in ('jumpAbsolute', 'popJumpIfTrue', 'popJumpIfFalse', 'jumpIfTrueOrPop', 'jumpIfFalseOrPop'):
+            value = int(args).__floordiv__(2)
+            args = f'label: CodeObject.Label(jumpAddress: {value})'
+
+        elif name == 'formatValue':
+            pass  # TODO: formatValue
+
+        elif name == 'buildString':
+            args = 'elementCount: Int):' + args
+        elif name == 'buildSlice':
+            pass  # TODO: buildSlice
+
+        if args:
+            args = '(' + args + ')'
+        comma = '' if name == 'return' else ','
+        print(f'.{name}{args}{comma}')
 
 
 def to_camel_case(snake_str):
