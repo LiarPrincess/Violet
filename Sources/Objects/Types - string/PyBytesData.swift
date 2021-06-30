b3e6474fc1532c60c53379e933a5e5a9561cfcf4
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2021-06-30 13:17:40 +0200

    Moved 'PyBytesType' to separate file

--- Sources/Objects/Types - string/PyBytesData.swift
+++ Sources/Objects/Types - string/PyBytesData.swift
@@ -5,16 +5,6 @@ import VioletCore
 // swiftlint:disable file_length
 // swiftlint:disable yoda_condition
 
-/// Protocol implemented by both `bytes` and `bytearray`.
-internal protocol PyBytesType: PyObject {
-  var data: PyBytesData { get }
-
-  /// Is this builtin `bytes/bytearray` type?
-  ///
-  /// Will return `false` if this is a subclass.
-  func checkExact() -> Bool
-}
-
 // MARK: - Bytes data
 
 /// Shared code between `PyBytes` and `PyBytesArray`.
