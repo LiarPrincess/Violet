ceb3c6c8122b3d888a4557f20074239955d84e94
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2021-07-25 10:20:32 +0200

    'Objects/Generated' script will now print more info

--- Sources/Objects/Generated/run.sh
+++ Sources/Objects/Generated/run.sh
@@ -10,11 +10,11 @@ GENERATED=./Sources/Objects/Generated
 # This stage has to be first because other stages will depend on newly created
 # type definitions.
 
-# === Builtin errors ===
-# This will add new error types (as subclasses of 'PyBaseException')
-echo 'Generating exception subclasses'
+echo "=== Builtin errors ==="
+echo "Add new error types (as subclasses of 'PyBaseException')."
+echo "- ExceptionSubclasses.swift"
 python3 $GENERATED/ExceptionSubclasses.py > $GENERATED/ExceptionSubclasses.swift
-echo ''
+echo
 
 # =====================================
 # Stage 2: Work on all type definitions
@@ -22,34 +22,34 @@ echo ''
 
 # This stage has to be after 'Stage 1: Generate new types'
 
-# === Dump types ===
-# This will generate a giant file wile with of all of the Python types/methods.
-# It will be used as a 'single source of truth' for later stages.
-echo 'Running Sourcery'
+echo "=== Sourcery ==="
+echo "Generate a giant file wile with of all of the types/methods."
+echo "It will be used as a 'single source of truth' for later stages."
+echo "- Sourcery/dump.txt"
 sourcery \
   --sources $OBJECTS \
   --templates $GENERATED/Sourcery/dump.stencil \
   --output $GENERATED/Sourcery/dump.txt \
   --quiet
-echo ''
+echo
 
-# === Builtin types ===
-# This will generate class that will create 'PyType' object
-# for each of the builtin types.
-echo 'Generating builtin type instances'
+echo "=== Builtin types ==="
+echo "This will generate a type that will init and store 'PyType' objects"
+echo "for each of the builtin types."
+echo "- BuiltinTypes.swift"
 python3 $GENERATED/BuiltinTypes.py > $GENERATED/BuiltinTypes.swift
+echo "- BuiltinErrorTypes.swift"
 python3 $GENERATED/BuiltinErrorTypes.py > $GENERATED/BuiltinErrorTypes.swift
+echo
 
-# === Type memory layout ===
-# When creating new class we will check if all of the base classes have
-# the same layout.
-# So, for example we will allow this:
-#   >>> class C(int, object): pass
-# but do not allow this:
-#   >>> class C(int, str): pass
-#   TypeError: multiple bases have instance lay-out conflict
-echo 'Generating type memory layouts'
+echo "=== Type memory layout ==="
+echo "When creating new class we will check if all of the base classes have"
+echo "the same layout."
+echo "So, for example we will allow this: 'class C(int, object): pass'"
+echo "But do not allow this: 'class C(int, str): pass'"
+echo "- TypeMemoryLayout.swift"
 python3 $GENERATED/TypeMemoryLayout.py > $GENERATED/TypeMemoryLayout.swift
+echo
 
 # ==============
 # Stage 3: Other
@@ -57,36 +57,40 @@ python3 $GENERATED/TypeMemoryLayout.py > $GENERATED/TypeMemoryLayout.swift
 
 # This stage does not really depend on type definitions.
 
-# === Casting ===
-# Sometimes we have to cast from 'PyObject' to specific Swift type.
-# This file generates casting methods.
-echo 'Generating PyCast (methods for casting PyObject -> specific Swift type)'
+echo "=== Casting ==="
+echo "Sometimes we have to cast from 'PyObject' to specific Swift type."
+echo "This file generates casting methods."
+echo "- PyCast.swift"
 python3 $GENERATED/PyCast.py > $GENERATED/PyCast.swift
+echo
 
-# === Memory ===
-# Helper for allocating new object instances.
-echo 'Generating PyMemory (helper for allocating new object instances)'
+echo "=== Memory ==="
+echo "Helper for allocating new object instances."
+echo "- PyMemory.swift"
 python3 $GENERATED/PyMemory.py > $GENERATED/PyMemory.swift
+echo
 
-# === Function wrappers ===
-# Helper for storing and calling Swift functions (regardless of their signature)
-echo 'Generating FunctionWrapper (helper for storing and calling Swift functions)'
+echo "=== Function wrappers ==="
+echo "Helper for storing and calling Swift functions (regardless of their signature)."
+echo "- FunctionWrapper - the actual type definition"
 python3 $GENERATED/FunctionWrapper.py > $GENERATED/FunctionWrapper.swift
-echo "Generating PyBuiltinFunction+Wrap (factory methods for 'builtinfunction')"
+echo "- PyBuiltinFunction+Wrap - factory methods for 'builtinfunction'"
 python3 $GENERATED/PyBuiltinFunction+Wrap.py > $GENERATED/PyBuiltinFunction+Wrap.swift
-echo "Generating PyClassMethod+Wrap (factory methods for 'classmethod')"
+echo "- PyClassMethod+Wrap - factory methods for 'classmethod'"
 python3 $GENERATED/PyClassMethod+Wrap.py > $GENERATED/PyClassMethod+Wrap.swift
+echo
 
-# === Static dispatch ===
-# Sometimes instead of doing slow Python dispatch we will use 'object.type.staticMethods'.
-echo "Generating PyStaticCall (call method via 'object.type.staticMethods' if possible)"
+echo "=== Static dispatch ==="
+echo "Sometimes instead of doing slow Python dispatch we will use 'object.type.staticMethods'."
+echo "- PyStaticCall.swift - interface for calling static methods"
 python3 $GENERATED/PyStaticCall.py > $GENERATED/PyStaticCall.swift
-echo 'Generating StaticMethodsForBuiltinTypes (static methods defined on builtin types)'
+echo "- StaticMethodsForBuiltinTypes.swift - static methods defined on builtin types"
 python3 $GENERATED/StaticMethodsForBuiltinTypes.py > $GENERATED/StaticMethodsForBuiltinTypes.swift
+echo
 
-# === IdString ===
-# Predefined commonly used `__dict__` keys.
-# Similar to `_Py_IDENTIFIER` in `CPython`.
-echo "Generating IdStrings (predefined commonly used '__dict__' keys)"
+echo "=== IdString ==="
+echo "Predefined commonly used '__dict__' keys."
+echo "Similar to '_Py_IDENTIFIER' in CPython."
+echo "- IdStrings.swift"
 python3 $GENERATED/IdStrings.py > $GENERATED/IdStrings.swift
-echo ''
+echo
