9058025e31f9abf7d5060af4ea8d1f0a381d7933
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-02-28 11:17:08 +0100

    Added 'SizeOf' and 'AlignmentOf'

---
+++ Sources/Objects/Memory/AlignmentOf.swift
@@ -0,0 +1,58 @@
+import Foundation
+import BigInt
+import VioletCore
+
+/// Alignment of a various things in a memory.
+public enum AlignmentOf {
+
+  // We only have 1 possible alignment for all of the Python objects.
+  // It is important it is a compile-time constant!
+
+  public static let word = SizeOf.object
+
+  public static func roundUp(_ value: Int, to alignment: Int) -> Int {
+    let rem = value % alignment
+    return rem == 0 ? value : value - rem + alignment
+  }
+
+  internal static func checkInvariants() {
+    Self.checkAlignment(of: RawPtr.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: Ptr<PyObject>.self, isLessEqual: AlignmentOf.word)
+
+    Self.checkAlignment(of: Int32.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: Int64.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: UInt32.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: UInt64.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: BigInt.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: Double.self, isLessEqual: AlignmentOf.word)
+
+    Self.checkAlignment(of: [PyObject].self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: String.self, isLessEqual: AlignmentOf.word)
+    Self.checkAlignment(of: Data.self, isLessEqual: AlignmentOf.word)
+
+    Self.checkAlignment(of: PyType.DeinitializerFn.self, isLessEqual: AlignmentOf.word)
+
+    // Self.checkAlignment(of: BlockStack.self, isLessEqual: Self.BlockStack)
+    // Self.checkAlignment(of: CodeObject.self, isLessEqual: Self.CodeObject)
+    // Self.checkAlignment(of: FileDescriptorType.self, isLessEqual: Self.FileDescriptorType)
+    // Self.checkAlignment(of: FileMode.self, isLessEqual: Self.FileMode)
+    // Self.checkAlignment(of: FunctionWrapper.self, isLessEqual: Self.FunctionWrapper)
+    // Self.checkAlignment(of: MemoryLayout.self, isLessEqual: Self.MemoryLayout)
+    // Self.checkAlignment(of: ObjectStack.self, isLessEqual: Self.ObjectStack)
+    // Self.checkAlignment(of: OrderedDictionary.self, isLessEqual: Self.OrderedDictionary)
+    // Self.checkAlignment(of: OrderedSet.self, isLessEqual: Self.OrderedSet)
+    // Self.checkAlignment(of: PyAnySet.self, isLessEqual: Self.PyAnySet)
+    // Self.checkAlignment(of: PyString.Encoding.self, isLessEqual: Self.PyString.Encoding)
+    // Self.checkAlignment(of: PyString.ErrorHandling.self, isLessEqual: Self.PyString.ErrorHandling)
+    // Self.checkAlignment(of: SourceLine.self, isLessEqual: Self.SourceLine)
+    // Self.checkAlignment(of: StaticallyKnownNotOverriddenMethods.self, isLessEqual: Self.StaticallyKnownNotOverriddenMethods)
+  }
+
+  private static func checkAlignment<T>(of type: T.Type, isLessEqual: Int) {
+    let alignment = MemoryLayout<T>.alignment
+    guard alignment <= isLessEqual else {
+      let typeName = String(describing: type)
+      trap("[Invariant] \(typeName) has alignment \(alignment) instead of expected \(isLessEqual)")
+    }
+  }
+}
---
+++ Sources/Objects/Memory/SizeOf.swift
@@ -0,0 +1,87 @@
+import Foundation
+import BigInt
+import VioletCore
+
+/// Sizes of a various things in a memory.
+public enum SizeOf {
+
+  // Alternatives:
+  // - use 'MemoryLayout<T>.stride' everywhere - this would make the 'mental math'
+  //   more difficult.
+  // - write general purpose layout algorithm - too difficult for what we gain.
+  //   The algorithm itself is quite simple, but then adding it to every type is not.
+  //
+  // Btw. It is important that those are a compile-time constants!
+
+  public static let object = 8
+  public static let objectHeader = PyObjectHeader.Layout.size
+
+  public static let int32 = 4
+  public static let int64 = 8
+  public static let uint32 = 4
+  public static let uint64 = 8
+  public static let bigInt = 8
+  public static let double = 8
+
+  public static let array = -1
+  public static let string = 16
+  public static let data = -1
+
+  public static let function = 16
+
+  // public static let BlockStack = -1
+  // public static let CodeObject = -1
+  // public static let FileDescriptorType = -1
+  // public static let FileMode = -1
+  // public static let FunctionWrapper = -1
+  // public static let MemoryLayout = -1
+  // public static let ObjectStack = -1
+  // public static let OrderedDictionary = -1
+  // public static let OrderedSet = -1
+  // public static let PyAnySet = -1
+  // public static let PyString.Encoding = -1
+  // public static let PyString.ErrorHandling = -1
+  // public static let SourceLine = -1
+  // public static let StaticallyKnownNotOverriddenMethods = -1
+
+  internal static func checkInvariants() {
+    Self.checkSize(of: RawPtr.self, expected: Self.object)
+    Self.checkSize(of: Ptr<PyObject>.self, expected: Self.object)
+
+    Self.checkSize(of: Int32.self, expected: Self.int32)
+    Self.checkSize(of: Int64.self, expected: Self.int64)
+    Self.checkSize(of: UInt32.self, expected: Self.uint32)
+    Self.checkSize(of: UInt64.self, expected: Self.uint64)
+    Self.checkSize(of: BigInt.self, expected: Self.bigInt)
+    Self.checkSize(of: Double.self, expected: Self.double)
+
+    Self.checkSize(of: [PyObject].self, expected: Self.array)
+    Self.checkSize(of: String.self, expected: Self.string)
+    Self.checkSize(of: Data.self, expected: Self.data)
+
+    Self.checkSize(of: PyType.DeinitializerFn.self, expected: Self.function)
+
+    // Self.checkSize(of: BlockStack.self, expected: Self.BlockStack)
+    // Self.checkSize(of: CodeObject.self, expected: Self.CodeObject)
+    // Self.checkSize(of: FileDescriptorType.self, expected: Self.FileDescriptorType)
+    // Self.checkSize(of: FileMode.self, expected: Self.FileMode)
+    // Self.checkSize(of: FunctionWrapper.self, expected: Self.FunctionWrapper)
+    // Self.checkSize(of: MemoryLayout.self, expected: Self.MemoryLayout)
+    // Self.checkSize(of: ObjectStack.self, expected: Self.ObjectStack)
+    // Self.checkSize(of: OrderedDictionary.self, expected: Self.OrderedDictionary)
+    // Self.checkSize(of: OrderedSet.self, expected: Self.OrderedSet)
+    // Self.checkSize(of: PyAnySet.self, expected: Self.PyAnySet)
+    // Self.checkSize(of: PyString.Encoding.self, expected: Self.PyString.Encoding)
+    // Self.checkSize(of: PyString.ErrorHandling.self, expected: Self.PyString.ErrorHandling)
+    // Self.checkSize(of: SourceLine.self, expected: Self.SourceLine)
+    // Self.checkSize(of: StaticallyKnownNotOverriddenMethods.self, expected: Self.StaticallyKnownNotOverriddenMethods)
+  }
+
+  private static func checkSize<T>(of type: T.Type, expected: Int) {
+    let size = MemoryLayout<T>.stride
+    if size != expected {
+      let typeName = String(describing: type)
+      trap("[Invariant] \(typeName) has size \(size) instead of expected \(expected)")
+    }
+  }
+}
