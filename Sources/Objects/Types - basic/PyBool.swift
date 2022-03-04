d3ca8b7196944dcf20bfd5e1fe432c6801b19731
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-03-04 12:02:01 +0100

    Restored bool

--- Sources/Objects/Types - basic/PyBool.swift
+++ Sources/Objects/Types - basic/PyBool.swift
@@ -20,7 +20,6 @@ extension BigInt {
 public struct PyBool: PyObjectMixin {
 
   // sourcery: pytypedoc
-  // Why 'Bool' suffix? See comment at the top of this file.
   internal static let doc = """
     bool(x) -> bool
 
@@ -50,124 +49,186 @@ public struct PyBool: PyObjectMixin {
   // Nothing to do here.
   internal func beforeDeinitialize() { }
 
-  // MARK: - Debug
-
   internal static func createDebugString(ptr: RawPtr) -> String {
     let zelf = PyBool(ptr: ptr)
     let value = zelf.value
     return "PyBool(type: \(zelf.typeName), flags: \(zelf.flags), value: \(value))"
   }
-}
-
-/* MARKER
 
   // MARK: - String
 
   // sourcery: pymethod = __repr__
-  internal func reprBool() -> String {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    return self.isTrue ? "True" : "False"
+  internal static func __repr__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    return Self.toString(py, zelf: zelf, fnName: "__repr__")
   }
 
   // sourcery: pymethod = __str__
-  internal func strBool() -> String {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    return self.isTrue ? "True" : "False"
+  internal static func __str__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    return Self.toString(py, zelf: zelf, fnName: "__str__")
+  }
+
+  private static func toString(_ py: Py,
+                               zelf: PyObject,
+                               fnName: String) -> PyResult<PyObject> {
+    guard let zelf = Self.castZelf(py, zelf) else {
+      return Self.invalidSelfArgument(py, zelf, fnName: fnName)
+    }
+
+    let result = zelf.isTrue ?
+    py.intern(string: "True") :
+    py.intern(string: "False")
+
+    return .value(result.asObject)
   }
 
   // MARK: - Class
 
   // sourcery: pyproperty = __class__
-  internal func getClassBool() -> PyType {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    return self.type
+  internal static func __class__(_ py: Py, zelf: PyObject) -> PyType {
+    return zelf.type
   }
 
   // MARK: - And
 
   // sourcery: pymethod = __and__
-  internal func andBool(_ other: PyObject) -> PyResult<PyObject> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    if let other = PyCast.asBool(other) {
-      let result = self.isTrue && other.isTrue
-      return .value(Py.newBool(result))
-    }
-
-    // Call method from 'int'
-    return self.and(other)
+  internal static func __and__(_ py: Py,
+                               zelf: PyObject,
+                               other: PyObject) -> PyResult<PyObject> {
+    return Self.binaryOperation(py,
+                                zelf: zelf,
+                                other: other,
+                                fnName: "__and__",
+                                fn: { $0 && $1 },
+                                intFn: PyInt.__add__(_:zelf:other:))
   }
 
   // sourcery: pymethod = __rand__
-  internal func randBool(_ other: PyObject) -> PyResult<PyObject> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    return self.andBool(other)
+  internal static func __rand__(_ py: Py,
+                                zelf: PyObject,
+                                other: PyObject) -> PyResult<PyObject> {
+    return Self.binaryOperation(py,
+                                zelf: zelf,
+                                other: other,
+                                fnName: "__rand__",
+                                fn: { $1 && $0 },
+                                intFn: PyInt.__radd__(_:zelf:other:))
   }
 
   // MARK: - Or
 
   // sourcery: pymethod = __or__
-  internal func orBool(_ other: PyObject) -> PyResult<PyObject> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    if let other = PyCast.asBool(other) {
-      let result = self.isTrue || other.isTrue
-      return .value(Py.newBool(result))
-    }
-
-    // Call method from 'int'
-    return self.or(other)
+  internal static func __or__(_ py: Py,
+                              zelf: PyObject,
+                              other: PyObject) -> PyResult<PyObject> {
+    return Self.binaryOperation(py,
+                                zelf: zelf,
+                                other: other,
+                                fnName: "__or__",
+                                fn: { $0 || $1 },
+                                intFn: PyInt.__or__(_:zelf:other:))
   }
 
   // sourcery: pymethod = __ror__
-  internal func rorBool(_ other: PyObject) -> PyResult<PyObject> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    return self.orBool(other)
+  internal static func __ror__(_ py: Py,
+                               zelf: PyObject,
+                               other: PyObject) -> PyResult<PyObject> {
+    return Self.binaryOperation(py,
+                                zelf: zelf,
+                                other: other,
+                                fnName: "__ror__",
+                                fn: { $1 || $0 },
+                                intFn: PyInt.__ror__(_:zelf:other:))
   }
 
   // MARK: - Xor
 
   // sourcery: pymethod = __xor__
-  internal func xorBool(_ other: PyObject) -> PyResult<PyObject> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    if let other = PyCast.asBool(other) {
-      let result = self.isTrue != other.isTrue
-      return .value(Py.newBool(result))
-    }
-
-    // Call method from 'int'
-    return self.xor(other)
+  internal static func __xor__(_ py: Py,
+                               zelf: PyObject,
+                               other: PyObject) -> PyResult<PyObject> {
+    return Self.binaryOperation(py,
+                                zelf: zelf,
+                                other: other,
+                                fnName: "__xor__",
+                                fn: { $0 != $1 },
+                                intFn: PyInt.__xor__(_:zelf:other:))
   }
 
   // sourcery: pymethod = __rxor__
-  internal func rxorBool(_ other: PyObject) -> PyResult<PyObject> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-    return self.xorBool(other)
+  internal static func __rxor__(_ py: Py,
+                                zelf: PyObject,
+                                other: PyObject) -> PyResult<PyObject> {
+    return Self.binaryOperation(py,
+                                zelf: zelf,
+                                other: other,
+                                fnName: "__rxor__",
+                                fn: { $1 != $0 },
+                                intFn: PyInt.__rxor__(_:zelf:other:))
   }
 
   // MARK: - Python new
 
   // sourcery: pystaticmethod = __new__
-  internal static func pyNewBool(type: PyType,
-                                 args: [PyObject],
-                                 kwargs: PyDict?) -> PyResult<PyBool> {
-    // Why 'Bool' suffix? See comment at the top of this file.
-
-    if let e = ArgumentParser.noKwargsOrError(fnName: "bool", kwargs: kwargs) {
-      return .error(e)
+  internal static func __new__(_ py: Py,
+                               type: PyType,
+                               args: [PyObject],
+                               kwargs: PyDict?) -> PyResult<PyObject> {
+    if let e = ArgumentParser.noKwargsOrError(py, fnName: "bool", kwargs: kwargs) {
+      return .error(e.asBaseException)
     }
 
-    if let e = ArgumentParser.guaranteeArgsCountOrError(fnName: "bool",
+    if let e = ArgumentParser.guaranteeArgsCountOrError(py,
+                                                        fnName: "bool",
                                                         args: args,
                                                         min: 0,
                                                         max: 1) {
-      return .error(e)
+      return .error(e.asBaseException)
     }
 
     if args.isEmpty {
-      return .value(Py.false)
+      let result = py.false
+      return .value(result.asObject)
     }
 
-    return Py.isTrue(object: args[0])
+    let result = py.isTrue(object: args[0])
+    return result.asObject
+  }
+
+  // MARK: - Helpers
+
+  private static func binaryOperation(
+    _ py: Py,
+    zelf: PyObject,
+    other: PyObject,
+    fnName: String,
+    fn: (Bool, Bool) -> Bool,
+    intFn: (Py, PyObject, PyObject) -> PyResult<PyObject>
+  ) -> PyResult<PyObject> {
+    guard let zelf = Self.castZelf(py, zelf) else {
+      return Self.invalidSelfArgument(py, zelf, fnName: fnName)
+    }
+
+    guard let other = py.cast.asBool(other) else {
+      return intFn(py, zelf.asObject, other)
+    }
+
+    let result = fn(zelf.isTrue, other.isTrue)
+    return result.toResult(py)
+  }
+
+  private static func castZelf(_ py: Py, _ object: PyObject) -> PyBool? {
+    return py.cast.asBool(object)
+  }
+
+  private static func invalidSelfArgument(
+    _ py: Py,
+    _ object: PyObject,
+    fnName: String
+  ) -> PyResult<PyObject> {
+    let error = py.newInvalidSelfArgumentError(object: object,
+                                               expectedType: "bool",
+                                               fnName: fnName)
+
+    return .error(error.asBaseException)
   }
 }
-
-*/
