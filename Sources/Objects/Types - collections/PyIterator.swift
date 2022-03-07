e9f6f5cd97ffb45996b2c0f7a9cc73d5d3d8481e
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-03-07 19:03:31 +0100

    Restored iterators

--- Sources/Objects/Types - collections/PyIterator.swift
+++ Sources/Objects/Types - collections/PyIterator.swift
@@ -12,10 +12,16 @@ public struct PyIterator: PyObjectMixin {
   // sourcery: pytypedoc
   internal static let doc: String? = nil
 
+  private static let endIndex = -1
+
   // sourcery: includeInLayout
   internal var sequence: PyObject { self.sequencePtr.pointee }
+
   // sourcery: includeInLayout
-  internal var index: Int { self.indexPtr.pointee }
+  internal var index: Int {
+    get { self.indexPtr.pointee }
+    nonmutating set { self.indexPtr.pointee = newValue }
+  }
 
   public let ptr: RawPtr
 
@@ -36,48 +42,59 @@ public struct PyIterator: PyObjectMixin {
     let zelf = PyIterator(ptr: ptr)
     return "PyIterator(type: \(zelf.typeName), flags: \(zelf.flags))"
   }
-}
-
-/* MARKER
 
   // MARK: - Class
 
   // sourcery: pyproperty = __class__
-  internal func getClass() -> PyType {
-    return self.type
+  internal static func __class__(_ py: Py, zelf: PyObject) -> PyType {
+    return zelf.type
   }
 
   // MARK: - Attributes
 
   // sourcery: pymethod = __getattribute__
-  internal func getAttribute(name: PyObject) -> PyResult<PyObject> {
-    return AttributeHelper.getAttribute(from: self, name: name)
+  internal static func __getattribute__(_ py: Py,
+                                        zelf: PyObject,
+                                        name: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__getattribute__")
+    }
+
+    return AttributeHelper.getAttribute(py, object: zelf.asObject, name: name)
   }
 
   // MARK: - Iter
 
   // sourcery: pymethod = __iter__
-  internal func iter() -> PyObject {
-    return self
+  internal static func __iter__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__iter__")
+    }
+
+    return PyResult(zelf)
   }
 
   // MARK: - Next
 
   // sourcery: pymethod = __next__
-  internal func next() -> PyResult<PyObject> {
-    guard self.index != PyIterator.endIndex else {
-      return .stopIteration()
+  internal static func __next__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__next__")
     }
 
-    switch Py.getItem(object: self.sequence, index: self.index) {
+    guard zelf.index != Self.endIndex else {
+      return .stopIteration(py)
+    }
+
+    switch py.getItem(object: zelf.sequence, index: zelf.index) {
     case .value(let o):
-      self.index += 1
+      zelf.index += 1
       return .value(o)
 
     case .error(let e):
-      if PyCast.isIndexError(e) || PyCast.isStopIteration(e) {
-        self.index = PyIterator.endIndex
-        return .stopIteration()
+      if py.cast.isIndexError(e.asObject) || py.cast.isStopIteration(e.asObject) {
+        zelf.index = Self.endIndex
+        return .stopIteration(py)
       }
 
       return .error(e)
@@ -87,16 +104,18 @@ public struct PyIterator: PyObjectMixin {
   // MARK: - Length hint
 
   // sourcery: pymethod = __length_hint__
-  internal func lengthHint() -> PyResult<PyInt> {
+  internal static func __length_hint__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__length_hint__")
+    }
+
     let len: BigInt
-    switch Py.lenBigInt(iterable: self.sequence) {
+    switch py.lengthBigInt(iterable: zelf.sequence) {
     case let .value(l): len = l
     case let .error(e): return .error(e)
     }
 
-    let result = len - BigInt(self.index)
-    return .value(Py.newInt(result))
+    let result = len - BigInt(zelf.index)
+    return PyResult(py, result)
   }
 }
-
-*/
--- Sources/Objects/Types - collections/PyReversed.swift
+++ Sources/Objects/Types - collections/PyReversed.swift
@@ -22,8 +22,12 @@ public struct PyReversed: PyObjectMixin {
 
   // sourcery: includeInLayout
   internal var sequence: PyObject { self.sequencePtr.pointee }
+
   // sourcery: includeInLayout
-  internal var index: Int { self.indexPtr.pointee }
+  internal var index: Int {
+    get { self.indexPtr.pointee }
+    nonmutating set { self.indexPtr.pointee = newValue }
+  }
 
   public let ptr: RawPtr
 
@@ -46,43 +50,54 @@ public struct PyReversed: PyObjectMixin {
     let zelf = PyReversed(ptr: ptr)
     return "PyReversed(type: \(zelf.typeName), flags: \(zelf.flags))"
   }
-}
-
-/* MARKER
 
   // MARK: - Class
 
   // sourcery: pyproperty = __class__
-  internal func getClass() -> PyType {
-    return self.type
+  internal static func __class__(_ py: Py, zelf: PyObject) -> PyType {
+    return zelf.type
   }
 
   // MARK: - Attributes
 
   // sourcery: pymethod = __getattribute__
-  internal func getAttribute(name: PyObject) -> PyResult<PyObject> {
-    return AttributeHelper.getAttribute(from: self, name: name)
+  internal static func __getattribute__(_ py: Py,
+                                        zelf: PyObject,
+                                        name: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__getattribute__")
+    }
+
+    return AttributeHelper.getAttribute(py, object: zelf.asObject, name: name)
   }
 
   // MARK: - Iter
 
   // sourcery: pymethod = __iter__
-  internal func iter() -> PyObject {
-    return self
+  internal static func __iter__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__iter__")
+    }
+
+    return PyResult(zelf)
   }
 
   // MARK: - Next
 
   // sourcery: pymethod = __next__
-  internal func next() -> PyResult<PyObject> {
-    if self.index != PyReversed.endIndex {
-      switch Py.getItem(object: self.sequence, index: self.index) {
+  internal static func __next__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__next__")
+    }
+
+    if zelf.index != PyReversed.endIndex {
+      switch py.getItem(object: zelf.sequence, index: zelf.index) {
       case .value(let o):
-        self.index -= 1
+        zelf.index -= 1
         return .value(o)
 
       case .error(let e):
-        if PyCast.isIndexError(e) || PyCast.isStopIteration(e) {
+        if py.cast.isIndexError(e.asObject) || py.cast.isStopIteration(e.asObject) {
           break
         }
 
@@ -90,82 +105,91 @@ public struct PyReversed: PyObjectMixin {
       }
     }
 
-    self.index = PyReversed.endIndex
-    return .stopIteration()
+    zelf.index = PyReversed.endIndex
+    return .stopIteration(py)
   }
 
   // MARK: - Length hint
 
   // sourcery: pymethod = __length_hint__
-  internal func lengthHint() -> PyInt {
+  internal static func __length_hint__(_ py: Py, zelf: PyObject) -> PyResult<PyObject> {
+    guard let zelf = Self.downcast(py, zelf) else {
+      return Self.invalidZelfArgument(py, zelf, "__length_hint__")
+    }
+
     // '+1' because users start counting from 1, not from 0
-    return Py.newInt(self.index + 1)
+    let result = zelf.index + 1
+    return PyResult(py, result)
   }
 
   // MARK: - Python new
 
   // sourcery: pystaticmethod = __new__
-  internal static func pyNew(type: PyType,
-                             args: [PyObject],
-                             kwargs: PyDict?) -> PyResult<PyObject> {
-    if let e = ArgumentParser.noKwargsOrError(fnName: "reversed", kwargs: kwargs) {
-      return .error(e)
+  internal static func __new__(_ py: Py,
+                               type: PyType,
+                               args: [PyObject],
+                               kwargs: PyDict?) -> PyResult<PyObject> {
+    if let e = ArgumentParser.noKwargsOrError(py,
+                                              fnName: Self.pythonTypeName,
+                                              kwargs: kwargs) {
+      return .error(e.asBaseException)
     }
 
-    if let e = ArgumentParser.guaranteeArgsCountOrError(fnName: "reversed",
+    if let e = ArgumentParser.guaranteeArgsCountOrError(py,
+                                                        fnName: Self.pythonTypeName,
                                                         args: args,
                                                         min: 1,
                                                         max: 1) {
-      return .error(e)
+      return .error(e.asBaseException)
     }
 
-    return PyReversed.pyNew(type: type, object: args[0])
+    return Self.__new__(py, type: type, object: args[0])
   }
 
-  private static func pyNew(type: PyType,
-                            object: PyObject) -> PyResult<PyObject> {
+  private static func __new__(_ py: Py,
+                              type: PyType,
+                              object: PyObject) -> PyResult<PyObject> {
     // If we have dedicated '__reversed__' then we will use it
-    switch PyReversed.call__reversed__(object: object) {
+    switch Self.call__reversed__(py, object: object) {
     case .value(let r):
       return .value(r)
     case .missingMethod:
       break // try other options
     case .error(let e),
-         .notCallable(let e):
+        .notCallable(let e):
       return .error(e)
     }
 
     // If we have '__getitem__' and '__len__' the we can manually create reverse.
-    switch PyReversed.has__getitem__(object: object) {
+    switch PyReversed.has__getitem__(py, object: object) {
     case .value(true):
       break // yay!
     case .value(false):
-      return .typeError("'\(object.typeName)' object is not reversible")
+      let message = "'\(object.typeName)' object is not reversible"
+      return .typeError(py, message: message)
     case .error(let e):
       return .error(e)
     }
 
     let count: Int
-    switch Py.lenInt(iterable: object) {
+    switch py.lengthInt(iterable: object) {
     case let .value(l): count = l
     case let .error(e): return .error(e)
     }
 
-    let result = PyMemory.newReversed(type: type, sequence: object, count: count)
-    return .value(result)
+    let result = py.memory.newReversed(py, type: type, sequence: object, count: count)
+    return PyResult(result)
   }
 
-  private static func call__reversed__(object: PyObject) -> PyInstance.CallMethodResult {
-    if let result = PyStaticCall.__reversed__(object) {
+  private static func call__reversed__(_ py: Py, object: PyObject) -> Py.CallMethodResult {
+    if let result = PyStaticCall.__reversed__(py, object: object) {
       return .value(result)
     }
 
-    return Py.callMethod(object: object, selector: .__reversed__)
+    return py.callMethod(object: object, selector: .__reversed__)
   }
 
-  private static func has__getitem__(object: PyObject) -> PyResult<Bool> {
-    return Py.hasMethod(object: object, selector: .__getitem__)
+  private static func has__getitem__(_ py: Py, object: PyObject) -> PyResult<Bool> {
+    return py.hasMethod(object: object, selector: .__getitem__)
   }
 }
-
-*/
