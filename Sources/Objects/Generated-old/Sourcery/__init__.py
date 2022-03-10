78d3f8717e879128246893acfff5236f99512313
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-03-10 21:24:08 +0100

    Renamed generated dirs in objects

---
+++ Sources/Objects/Generated-old/Sourcery/__init__.py
@@ -0,0 +1,2 @@
+from Sourcery.entities import TypeInfo, SwiftFieldInfo, SwiftFunctionInfo, PyPropertyInfo, PyFunctionInfo
+from Sourcery.get_types import get_types
--- Sources/Objects/Generated2/Sourcery/__init__.py
+++
@@ -1,8 +0,0 @@
-from Sourcery.ObjectHeader import ObjectHeader, ObjectHeaderField
-from Sourcery.ObjectHeader_get import get_object_header
-
-from Sourcery.ErrorHeader import ErrorHeader, ErrorHeaderField
-from Sourcery.ErrorHeader_get import get_error_header
-
-from Sourcery.TypeInfo import TypeInfo, SwiftFieldInMemoryInfo, SwiftInitializerInfo
-from Sourcery.TypeInfo_get import get_types
