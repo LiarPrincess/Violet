d1b0af6ed09b05b5e2b1caa2ef93baf190116f01
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-03-14 07:27:16 +0100

    Restored py mini-functions

--- Sources/Objects/Py+Thingies/Py+Bin+Hex+Oct.swift
+++ Sources/Objects/Py+Thingies/Py+Bin+Hex+Oct.swift
@@ -1,39 +1,40 @@
-/* MARKER
-extension PyInstance {
+extension Py {
 
   /// bin(x)
   /// See [this](https://docs.python.org/3/library/functions.html#bin)
-  public func bin(object: PyObject) -> PyResult<PyObject> {
+  public func bin(object: PyObject) -> PyResult<PyString> {
     return self.toNumberString(object: object, radix: 2, prefix: "0b")
   }
 
   /// oct(x)
   /// See [this](https://docs.python.org/3/library/functions.html#oct)
-  public func oct(object: PyObject) -> PyResult<PyObject> {
+  public func oct(object: PyObject) -> PyResult<PyString> {
     return self.toNumberString(object: object, radix: 8, prefix: "0o")
   }
 
   /// hex(x)
   /// See [this](https://docs.python.org/3/library/functions.html#hex)
-  public func hex(object: PyObject) -> PyResult<PyObject> {
+  public func hex(object: PyObject) -> PyResult<PyString> {
     return self.toNumberString(object: object, radix: 16, prefix: "0x")
   }
 
   private func toNumberString(object: PyObject,
                               radix: Int,
-                              prefix: String) -> PyResult<PyObject> {
-    switch IndexHelper.bigInt(object) {
-    case let .value(bigInt):
+                              prefix: String) -> PyResult<PyString> {
+    switch IndexHelper.pyInt(self, object: object) {
+    case let .value(pyInt):
+      let bigInt = pyInt.value
       let sign = bigInt < 0 ? "-" : ""
 
       let bigIntAbs = Swift.abs(bigInt)
       let number = String(bigIntAbs, radix: radix, uppercase: false)
 
       let result = sign + prefix + number
-      return .value(self.newString(result))
+      let resultObject = self.newString(result)
+      return .value(resultObject)
 
     case let .notIndex(lazyError):
-      let e = lazyError.create()
+      let e = lazyError.create(self)
       return .error(e)
 
     case let .error(e):
@@ -41,5 +42,3 @@ extension PyInstance {
     }
   }
 }
-
-*/
