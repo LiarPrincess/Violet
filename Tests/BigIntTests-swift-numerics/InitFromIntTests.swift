d10a7ef31682757ce85e1246608997f04053375f
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-12-30 17:38:09 +0100

    Added tests

---
+++ Tests/BigIntTests-swift-numerics/InitFromIntTests.swift
@@ -0,0 +1,392 @@
+//===--- InitFromIntTests.swift -------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift Numerics open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift Numerics project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import BigIntModule
+
+class InitFromIntTests: XCTestCase {
+
+  // MARK: - Exactly
+
+  func test_exactly_signed() {
+    self.exactly_inRange(type: Int8.self)
+    self.exactly_inRange(type: Int16.self)
+    self.exactly_inRange(type: Int32.self)
+    self.exactly_inRange(type: Int64.self)
+    self.exactly_inRange(type: Int.self)
+  }
+
+  func test_exactly_unsigned() {
+    self.exactly_inRange(type: UInt8.self)
+    self.exactly_inRange(type: UInt16.self)
+    self.exactly_inRange(type: UInt32.self)
+    self.exactly_inRange(type: UInt64.self)
+    self.exactly_inRange(type: UInt.self)
+  }
+
+  private func exactly_inRange<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    var cases: [T] = [0, 42, T.max, T.max - 1, T.min, T.min + 1]
+    cases.append(contentsOf: PositivePowersOf2(type: T.self).map { $0.value })
+
+    if T.isSigned {
+      cases.append(-1)
+      cases.append(-42)
+      cases.append(contentsOf: NegativePowersOf2(type: T.self).map { $0.value })
+    }
+
+    let typeName = String(describing: T.self)
+
+    for int in cases {
+      let big = BigInt(int)
+
+      // String representation should be equal - trivial test for value
+      let intString = String(int, radix: 10, uppercase: false)
+      let bigString = String(big, radix: 10, uppercase: false)
+      XCTAssertEqual(bigString, intString, "String(radix:)", file: file, line: line)
+
+      // T -> BigInt -> T
+      let revert = T(exactly: big)
+      XCTAssertEqual(int,
+                     revert,
+                     "\(typeName) -> BigInt -> \(typeName)(exactly:)",
+                     file: file,
+                     line: line)
+    }
+  }
+
+  func test_exactly_signed_moreThanMax() {
+    self.exactly_moreThanMax(type: Int8.self)
+    self.exactly_moreThanMax(type: Int16.self)
+    self.exactly_moreThanMax(type: Int32.self)
+    self.exactly_moreThanMax(type: Int64.self)
+    self.exactly_moreThanMax(type: Int.self)
+  }
+
+  func test_exactly_unsigned_moreThanMax() {
+    self.exactly_moreThanMax(type: UInt8.self)
+    self.exactly_moreThanMax(type: UInt16.self)
+    self.exactly_moreThanMax(type: UInt32.self)
+    self.exactly_moreThanMax(type: UInt64.self)
+    self.exactly_moreThanMax(type: UInt.self)
+  }
+
+  private func exactly_moreThanMax<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    let max = BigInt(type.max)
+    let cases = [1, 3, 7, 23, max, 2 * max - 3]
+
+    for n in cases {
+      let moreThanMax = max + n
+      let exactly = T(exactly: moreThanMax)
+      XCTAssertNil(exactly, "max + \(n)", file: file, line: line)
+    }
+  }
+
+  func test_exactly_signed_lessThanMin() {
+    self.exactly_lessThanMin(type: Int8.self)
+    self.exactly_lessThanMin(type: Int16.self)
+    self.exactly_lessThanMin(type: Int32.self)
+    self.exactly_lessThanMin(type: Int64.self)
+    self.exactly_lessThanMin(type: Int.self)
+  }
+
+  func test_exactly_unsigned_lessThanMin() {
+    self.exactly_lessThanMin(type: UInt8.self)
+    self.exactly_lessThanMin(type: UInt16.self)
+    self.exactly_lessThanMin(type: UInt32.self)
+    self.exactly_lessThanMin(type: UInt64.self)
+    self.exactly_lessThanMin(type: UInt.self)
+  }
+
+  private func exactly_lessThanMin<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    let min = BigInt(type.min)
+    let max = BigInt(type.max)
+    let cases = [1, 3, 7, 23, max, 2 * max - 3]
+
+    for n in cases {
+      let lessThanMin = min - n
+      let exactly = T(exactly: lessThanMin)
+      XCTAssertNil(exactly, "min - \(n)", file: file, line: line)
+    }
+  }
+
+  // MARK: - Clamping
+
+  func test_clamping_signed() {
+    self.clamping_inRange(type: Int8.self)
+    self.clamping_inRange(type: Int16.self)
+    self.clamping_inRange(type: Int32.self)
+    self.clamping_inRange(type: Int64.self)
+    self.clamping_inRange(type: Int.self)
+  }
+
+  func test_clamping_unsigned() {
+    self.clamping_inRange(type: UInt8.self)
+    self.clamping_inRange(type: UInt16.self)
+    self.clamping_inRange(type: UInt32.self)
+    self.clamping_inRange(type: UInt64.self)
+    self.clamping_inRange(type: UInt.self)
+  }
+
+  private func clamping_inRange<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+
+    var cases: [T] = [0, 42, T.max, T.max - 1, T.min, T.min + 1]
+    cases.append(contentsOf: PositivePowersOf2(type: T.self).map { $0.value })
+
+    if T.isSigned {
+      cases.append(-1)
+      cases.append(-42)
+      cases.append(contentsOf: NegativePowersOf2(type: T.self).map { $0.value })
+    }
+
+    let typeName = String(describing: T.self)
+
+    for int in cases {
+      let big = BigInt(int)
+
+      // String representation should be equal - trivial test for value
+      let intString = String(int, radix: 10, uppercase: false)
+      let bigString = String(big, radix: 10, uppercase: false)
+      XCTAssertEqual(bigString, intString, "String(radix:)", file: file, line: line)
+
+      // T -> BigInt -> T
+      let revert = T(clamping: big)
+      XCTAssertEqual(int,
+                     revert,
+                     "\(typeName) -> BigInt -> \(typeName)(clamping:)",
+                     file: file,
+                     line: line)
+    }
+  }
+
+  func test_clamping_signed_moreThanMax() {
+    self.clamping_moreThanMax(type: Int8.self)
+    self.clamping_moreThanMax(type: Int16.self)
+    self.clamping_moreThanMax(type: Int32.self)
+    self.clamping_moreThanMax(type: Int64.self)
+    self.clamping_moreThanMax(type: Int.self)
+  }
+
+  func test_clamping_unsigned_moreThanMax() {
+    self.clamping_moreThanMax(type: UInt8.self)
+    self.clamping_moreThanMax(type: UInt16.self)
+    self.clamping_moreThanMax(type: UInt32.self)
+    self.clamping_moreThanMax(type: UInt64.self)
+    self.clamping_moreThanMax(type: UInt.self)
+  }
+
+  private func clamping_moreThanMax<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    let maxT = type.max
+    let max = BigInt(maxT)
+    let cases = [1, 3, 7, 23, max, 2 * max - 3]
+
+    for n in cases {
+      let moreThanMax = max + n
+      let clamped = T(clamping: moreThanMax)
+      XCTAssertEqual(clamped, maxT, "max + \(n)", file: file, line: line)
+    }
+  }
+
+  func test_clamping_signed_lessThanMin() {
+    self.clamping_lessThanMin(type: Int8.self)
+    self.clamping_lessThanMin(type: Int16.self)
+    self.clamping_lessThanMin(type: Int32.self)
+    self.clamping_lessThanMin(type: Int64.self)
+    self.clamping_lessThanMin(type: Int.self)
+  }
+
+  func test_clamping_unsigned_lessThanMin() {
+    self.clamping_lessThanMin(type: UInt8.self)
+    self.clamping_lessThanMin(type: UInt16.self)
+    self.clamping_lessThanMin(type: UInt32.self)
+    self.clamping_lessThanMin(type: UInt64.self)
+    self.clamping_lessThanMin(type: UInt.self)
+  }
+
+  private func clamping_lessThanMin<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    let minT = type.min
+    let min = BigInt(minT)
+    let max = BigInt(type.max)
+    let cases = [1, 3, 7, 23, max, 2 * max - 3]
+
+    for n in cases {
+      let lessThanMin = min - n
+      let clamped = T(clamping: lessThanMin)
+      XCTAssertEqual(clamped, minT, "min - \(n)", file: file, line: line)
+    }
+  }
+
+  // MARK: - Truncating if needed
+
+  func test_truncatingIfNeeded_signed() {
+    self.truncatingIfNeeded_inRange(type: Int8.self)
+    self.truncatingIfNeeded_inRange(type: Int16.self)
+    self.truncatingIfNeeded_inRange(type: Int32.self)
+    self.truncatingIfNeeded_inRange(type: Int64.self)
+    self.truncatingIfNeeded_inRange(type: Int.self)
+  }
+
+  func test_truncatingIfNeeded_unsigned() {
+    self.truncatingIfNeeded_inRange(type: UInt8.self)
+    self.truncatingIfNeeded_inRange(type: UInt16.self)
+    self.truncatingIfNeeded_inRange(type: UInt32.self)
+    self.truncatingIfNeeded_inRange(type: UInt64.self)
+    self.truncatingIfNeeded_inRange(type: UInt.self)
+  }
+
+  private func truncatingIfNeeded_inRange<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+
+    var cases: [T] = [0, 42, T.max, T.max - 1, T.min, T.min + 1]
+    cases.append(contentsOf: PositivePowersOf2(type: T.self).map { $0.value })
+
+    if T.isSigned {
+      cases.append(-1)
+      cases.append(-42)
+      cases.append(contentsOf: NegativePowersOf2(type: T.self).map { $0.value })
+    }
+
+    let typeName = String(describing: T.self)
+
+    for int in cases {
+      let big = BigInt(int)
+
+      // String representation should be equal - trivial test for value
+      let intString = String(int, radix: 10, uppercase: false)
+      let bigString = String(big, radix: 10, uppercase: false)
+      XCTAssertEqual(bigString, intString, "String(radix:)", file: file, line: line)
+
+      // T -> BigInt -> T
+      let revert = T(truncatingIfNeeded: big)
+      XCTAssertEqual(int,
+                     revert,
+                     "\(typeName) -> BigInt -> \(typeName)(truncatingIfNeeded:)",
+                     file: file,
+                     line: line)
+    }
+  }
+
+  func test_truncatingIfNeeded_signed_moreThanMax() {
+    self.truncatingIfNeeded_moreThanMax(type: Int8.self)
+    self.truncatingIfNeeded_moreThanMax(type: Int16.self)
+    self.truncatingIfNeeded_moreThanMax(type: Int32.self)
+    self.truncatingIfNeeded_moreThanMax(type: Int64.self)
+    self.truncatingIfNeeded_moreThanMax(type: Int.self)
+  }
+
+  func test_truncatingIfNeeded_unsigned_moreThanMax() {
+    self.truncatingIfNeeded_moreThanMax(type: UInt8.self)
+    self.truncatingIfNeeded_moreThanMax(type: UInt16.self)
+    self.truncatingIfNeeded_moreThanMax(type: UInt32.self)
+    self.truncatingIfNeeded_moreThanMax(type: UInt64.self)
+    self.truncatingIfNeeded_moreThanMax(type: UInt.self)
+  }
+
+  private func truncatingIfNeeded_moreThanMax<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    do {
+      // signed:   0111 + 1 =   1000 -> min
+      // unsigned: 1111 + 1 = 1 0000 -> 0
+      let value = BigInt(type.max) + 1
+      let truncated = T(truncatingIfNeeded: value)
+      let expected = T.isSigned ? type.min : type.zero
+      XCTAssertEqual(truncated, expected, "max + 1", file: file, line: line)
+    }
+
+    let max = BigInt(type.max)
+    let cases = [1, 3, 7, 23, max - 3, max]
+    let discarded = BigInt(1) << T.bitWidth
+
+    for n in cases {
+      let moreThanMax = discarded + n
+      let truncated = T(truncatingIfNeeded: moreThanMax)
+      let truncatedBig = BigInt(truncated)
+      XCTAssertEqual(truncatedBig, n, "1 << \(T.bitWidth) + \(n)", file: file, line: line)
+    }
+  }
+
+  func test_truncatingIfNeeded_signed_lessThanMin() {
+    self.truncatingIfNeeded_lessThanMin(type: Int8.self)
+    self.truncatingIfNeeded_lessThanMin(type: Int16.self)
+    self.truncatingIfNeeded_lessThanMin(type: Int32.self)
+    self.truncatingIfNeeded_lessThanMin(type: Int64.self)
+    self.truncatingIfNeeded_lessThanMin(type: Int.self)
+  }
+
+  func test_truncatingIfNeeded_unsigned_lessThanMin() {
+    self.truncatingIfNeeded_lessThanMin(type: UInt8.self)
+    self.truncatingIfNeeded_lessThanMin(type: UInt16.self)
+    self.truncatingIfNeeded_lessThanMin(type: UInt32.self)
+    self.truncatingIfNeeded_lessThanMin(type: UInt64.self)
+    self.truncatingIfNeeded_lessThanMin(type: UInt.self)
+  }
+
+  private func truncatingIfNeeded_lessThanMin<T: FixedWidthInteger>(
+    type: T.Type,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+    let min = BigInt(type.min)
+
+    do {
+      // signed:   1000 - 1 = 0111 = max
+      // unsigned: 0000 - 1 = 1111 = max
+      let value = min - 1
+      let truncated = T(truncatingIfNeeded: value)
+      XCTAssertEqual(truncated, type.max, "min - 1", file: file, line: line)
+    }
+
+    // signed:   -12 = (1111) 0100 = -12
+    // unsigned: -12 = (1111) 0100 = ?
+    let max = BigInt(type.max)
+    let cases = [1, 3, 7, 23, max - 3, max]
+    let discarded = BigInt(1) << T.bitWidth
+
+    for n in cases {
+      let lessThanMin = -(discarded + n)
+      assert(lessThanMin < min)
+
+      let truncated = T(truncatingIfNeeded: lessThanMin)
+      let complement = ~truncated + 1 // no overflow possible
+      let truncatedBig = BigInt(complement)
+      XCTAssertEqual(truncatedBig, n, "1 << \(T.bitWidth) + \(n)", file: file, line: line)
+    }
+  }
+}
---
+++ Tests/BigIntTests-swift-numerics/InitFromStringTests.swift
@@ -0,0 +1,319 @@
+//===--- InitFromStringTests.swift ----------------------------*- swift -*-===//
+//
+// This source file is part of the Swift Numerics open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift Numerics project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import BigIntModule
+
+private typealias TestSuite = StringTestCases.TestSuite
+private typealias TestCase = StringTestCases.TestCase
+private typealias BinaryTestCases = StringTestCases.Binary
+private typealias QuinaryTestCases = StringTestCases.Quinary
+private typealias OctalTestCases = StringTestCases.Octal
+private typealias DecimalTestCases = StringTestCases.Decimal
+private typealias HexTestCases = StringTestCases.Hex
+
+class InitFromStringTests: XCTestCase {
+
+  // MARK: - Empty
+
+  func test_empty_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_onlySign_withoutDigits_plus_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "+", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_onlySign_withoutDigits_minus_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "-", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  // MARK: - Zero
+
+  func test_zero_single() {
+    let zero = BigInt()
+
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "0", radix: radix)
+      XCTAssertEqual(n, zero, "Radix: \(radix)")
+    }
+  }
+
+  func test_zero_single_plus() {
+    let zero = BigInt()
+
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "+0", radix: radix)
+      XCTAssertEqual(n, zero, "Radix: \(radix)")
+    }
+  }
+
+  func test_zero_single_minus() {
+    let zero = BigInt()
+
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "-0", radix: radix)
+      XCTAssertEqual(n, zero, "Radix: \(radix)")
+    }
+  }
+
+  func test_zero_multiple() {
+    let zero = BigInt()
+    let input = String(repeating: "0", count: 42)
+
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: input, radix: radix)
+      XCTAssertEqual(n, zero)
+    }
+  }
+
+  // MARK: - Int -> String -> BigInt
+
+  func test_int_toString_toBigInt_binary() {
+    self.int_toString_toBigInt(radix: 2)
+  }
+
+  func test_int_toString_toBigInt_decimal() {
+    self.int_toString_toBigInt(radix: 10)
+  }
+
+  func test_int_toString_toBigInt_hex() {
+    self.int_toString_toBigInt(radix: 16)
+  }
+
+  func int_toString_toBigInt(radix: Int,
+                             file: StaticString = #file,
+                             line: UInt = #line) {
+    for int in generateInts(approximateCount: 100) {
+      let expected = BigInt(int)
+
+      let lowercase = String(int, radix: radix, uppercase: false)
+      let lowercaseResult = self.create(string: lowercase, radix: radix)
+      XCTAssertEqual(lowercaseResult,
+                     expected,
+                     "\(int), lowercase",
+                     file: file,
+                     line: line)
+
+      let uppercase = String(int, radix: radix, uppercase: true)
+      let uppercaseResult = self.create(string: uppercase, radix: radix)
+      XCTAssertEqual(uppercaseResult,
+                     expected,
+                     "\(int), uppercase",
+                     file: file,
+                     line: line)
+    }
+  }
+
+  // MARK: - Binary
+
+  func test_binary_singleWord() {
+    self.run(suite: BinaryTestCases.singleWord)
+  }
+
+  func test_binary_twoWords() {
+    self.run(suite: BinaryTestCases.twoWords)
+  }
+
+  // MARK: - Quinary
+
+  func test_quinary_singleWord() {
+    self.run(suite: QuinaryTestCases.singleWord)
+  }
+
+  func test_quinary_twoWords() {
+    self.run(suite: QuinaryTestCases.twoWords)
+  }
+
+  // MARK: - Octal
+
+  func test_octal_singleWord() {
+    self.run(suite: OctalTestCases.singleWord)
+  }
+
+  func test_octal_twoWords() {
+    self.run(suite: OctalTestCases.twoWords)
+  }
+
+  func test_octal_threeWords() {
+    self.run(suite: OctalTestCases.threeWords)
+  }
+
+  // MARK: - Decimal
+
+  func test_decimal_singleWord() {
+    self.run(suite: DecimalTestCases.singleWord)
+  }
+
+  func test_decimal_twoWords() {
+    self.run(suite: DecimalTestCases.twoWords)
+  }
+
+  func test_decimal_threeWords() {
+    self.run(suite: DecimalTestCases.threeWords)
+  }
+
+  func test_decimal_fourWords() {
+    self.run(suite: DecimalTestCases.fourWords)
+  }
+
+  // MARK: - Hex
+
+  func test_hex_singleWord() {
+    self.run(suite: HexTestCases.singleWord)
+  }
+
+  func test_hex_twoWords() {
+    self.run(suite: HexTestCases.twoWords)
+  }
+
+  func test_hex_threeWords() {
+    self.run(suite: HexTestCases.threeWords)
+  }
+
+  // MARK: - Underscore
+
+  func test_underscore_binary_fails() {
+    self.runUnderscore(suite: BinaryTestCases.twoWords)
+  }
+
+  func test_underscore_decimal_fails() {
+    self.runUnderscore(suite: DecimalTestCases.twoWords)
+  }
+
+  private func runUnderscore(suite: TestSuite,
+                             file: StaticString = #file,
+                             line: UInt = #line) {
+    for testCase in suite.cases {
+      let s = testCase.stringWithUnderscores
+      let n = self.create(string: s, radix: suite.radix)
+      XCTAssertNil(n, s)
+    }
+  }
+
+  // MARK: - Underscore placement
+
+  func test_underscore_prefix_withoutSign_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "_0101", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_underscore_before_plusSign_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "_+0101", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_underscore_before_minusSign_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "_+0101", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_underscore_after_plusSign_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "+_0101", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_underscore_after_minusSign_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "-_0101", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_underscore_suffix_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "0101_", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_underscore_double_fails() {
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "01__01", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  // MARK: - Invalid digit
+
+  func test_invalidDigit_emoji_fails() {
+    let emoji = "😊"
+
+    for radix in [2, 4, 7, 32] {
+      let n = self.create(string: "01\(emoji)01", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  func test_invalidDigit_biggerThanRadix_fails() {
+    let cases: [(Int, UnicodeScalar)] = [
+      (2, "2"),
+      (4, "4"),
+      (7, "7"),
+      (10, "a"),
+      (16, "g")
+    ]
+
+    for (radix, biggerThanRadix) in cases {
+      let n = self.create(string: "01\(biggerThanRadix)01", radix: radix)
+      XCTAssertNil(n, "Got: \(String(describing: n)), radix: \(radix)")
+    }
+  }
+
+  // MARK: - Helpers
+
+  /// Abstraction over `BigInt.init(_:radix:)`.
+  private func create(string: String, radix: Int) -> BigInt? {
+    return BigInt(string, radix: radix)
+  }
+
+  private func run(suite: TestSuite,
+                   file: StaticString = #file,
+                   line: UInt = #line) {
+    let radix = suite.radix
+
+    for testCase in suite.cases {
+      let input = testCase.string
+      let expected = testCase.create()
+      let expectedNegative = testCase.create(sign: .negative)
+
+      let lowercased = self.create(string: input.lowercased(), radix: radix)
+      XCTAssertEqual(lowercased, expected, "LOWERCASE " + input, file: file, line: line)
+
+      let uppercased = self.create(string: input.uppercased(), radix: radix)
+      XCTAssertEqual(uppercased, expected, "UPPERCASE " + input, file: file, line: line)
+
+      let plusSign = self.create(string: "+" + input, radix: radix)
+      XCTAssertEqual(plusSign, expected, "PLUS " + input, file: file, line: line)
+
+      assert(!testCase.isZero, "-0 should be handled differently")
+      let minusSign = self.create(string: "-" + input, radix: radix)
+      XCTAssertEqual(minusSign, expectedNegative, "MINUS " + input, file: file, line: line)
+    }
+  }
+}
